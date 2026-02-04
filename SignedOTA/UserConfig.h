b8954f855aea5da6560b8770d04d57963a9a9dad
DESKTOP-3N3C4CT\ok <julie08051@naver.com>
2026-02-04 16:44:27 +0900

    Firmware Update v3

--- SignedOTA/UserConfig.h
+++ SignedOTA/UserConfig.h
@@ -21,7 +21,7 @@ const char *version_url = "https://raw.githubusercontent.com/Fuzzyline-HAS2/"
                           "updated_neopixel_hyo/main/version.txt";
 
 // 3. 디버그 및 버전 정보
-#define CURRENT_FIRMWARE_VERSION 2
+#define CURRENT_FIRMWARE_VERSION 3
 
 // ==========================================
 #endif
